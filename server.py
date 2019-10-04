c408b0e8707522865c279efba9d8a3b23160a7ee
Alexander <realalexkov@gmail.com>
2019-10-04 15:35:16 +0600

    add comments, DB removed, ServerApp class to control basic operations, db_init moved to anot file

--- server.py
+++ server.py
@@ -1,197 +1,91 @@
+"""
+  *Server instance code
+"""
 
-import selectors
-import socket
-import psycopg2 as psql
-import threading
-import types
-import time
+from libs.sql_access_server import *
 import logging as log
+import asyncio
 import traceback
+import selectors
+import struct
+
 log.basicConfig(level=log.DEBUG)
+sel = selectors.DefaultSelector()
 
 
-class DB:
-    def __init__(self, host: str, db_name: str, 
-                 user: str, password: str, db_tables: tuple, port=5432):
-        """ DB class 
-        """
-        self.host = host
-        self.port = port
-        self.db_name = db_name
-        self.db_tables = db_tables
-        self.user = user
-        self.password = password
+class ServerApp:
+    def __init__(self, addr, data_base: DB):
+        self.addr = addr
+        self.data_base = db_init(data_base)
+        if self.data_base is None:
+            raise Exception(f'DB init error')
+        self.response = None
+        self.server = None
 
-    def connect(self):
-        self.connection = psql.connect(host=self.host, port=self.port, 
-                                       database=self.db_name, user=self.user,
-                                       password=self.password)
-        self.cursor = self.connection.cursor()
-        return self.connection
+    async def accept_wrapper(self, sock):
+        conn, addr = sock.accept()
+        print("accepted connection from", addr)
+        conn.setblocking(False)
+        message = Server(sel, conn, addr, self.data_base)
+        sel.register(conn, selectors.EVENT_READ, data=message)
 
-    def _is_db_online(self):
-        self.cursor.execute("select 1")
-
-    def _is_table_exists(self, table_name):
-        self.cursor().execute(f"SELECT * FROM information_schema.tables WHERE table_name={table_name};")
-        self.cursor().commit()
-        if self.cursor.fetchone()[0] == 1:
-            return True
-        return False
-
-    def _execute_sql(self, sql: str):
+    async def run_server(self):
+        lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        lsock.bind(self.addr)
+        lsock.listen()
+        log.debug(f"listening on {self.addr}")
+        lsock.setblocking(False)
+        sel.register(lsock, selectors.EVENT_READ, data=None)
         try:
-            self.cursor.execute(sql)
-            self.connection.commit()
-            return self.cursor.fetchone()
-        except(Exception, psql.DatabaseError) as Error:
-            log.debug(f"DB execution Error...\n{Error}")
-            return None
+            while True:
+                events = sel.select(timeout=None)
+                for key, mask in events:
+                    if key.data is None:
+                        await self.accept_wrapper(key.fileobj)
+                    else:
+                        message = key.data
+                        try:
+                            message.process_events(mask)
+                        except Exception as Error:
+                            print(
+                                "main: error: exception for",
+                                f"{message.addr}:\n{Error}",
+                            )
+                            message.close()
+        except Exception as Error:
+            log.error(Error)
+        finally:
+            sel.close()
 
-    def read_value(self, var_name, table_name):
-        sql = f'SELECT * FROM {table_name};'
-        return self._execute_sql(sql)
-
-    def insert_value(self, table_name: str, var_name, value):
-        """table_name = str - name of the table
-          *var_to_insert_value = str - name of the var
-          *value = ? - value
-          *out_var_name = - value which would be returned (id or uni_code)
-        """
-        if table_name in self.db_tables:
-            
-            sql = f"""INSERT INTO {table_name}({var_name}) VALUES ({value});"""
-            return self._execute_sql(sql)
-
-    def fill_row(self):
-        return
-
-    def read_row(self):
-        return
-
-    def delete_table(self, table_name):
-        self._execute_sql(f'DROP TABLE IF EXISTS "{table_name}" CASCADE;')
-        return True
-
-    def create_table(self, table_name: str, vars: dict):
-        """create table
-            table_name : str
-            vars : dict - {"id" : "INT PRIMARY KEY     NOT NULL", "NAME" : "TEXT", AGE - INT}
-        """
-        if table_name in self.db_tables:
-            sql = f'CREATE TABLE IF NOT EXISTS {table_name} ('
-            current_key = 1
-            for key in vars:
-                sql += f"{key} {vars[key]}"
-                if current_key < len(vars):
-                    sql += ","
-                elif current_key == len(vars):
-                    sql += ");"
-                current_key += 1
-            self._execute_sql(sql)
-            return True
-        else:
-            return False
-
-
-def db_init(data_base: DB):
-    
-    data_base.connect()
-    try:
-        for table in data_base.db_tables:
-            data_base.delete_table(table)
-
-        data_base.create_table('organization', { 
-                        'id': "SERIAL PRIMARY KEY",
-                        'name': "TEXT NOT NULL",
-                        'uni_code': "INT NOT NULL",
-                        'department_uni_codes': "integer[]"})   
-        data_base.create_table('department', {
-                        'id': "SERIAL PRIMARY KEY",    
-                        'name': "TEXT NOT NULL",
-                        'uni_code': "INT NOT NULL",
-                        'persons_uni_codes': "integer[]"})
-        data_base.create_table('person', {
-                        'id': "SERIAL PRIMARY KEY",
-                        'name': "TEXT NOT NULL",
-                        'birth_day': "DATE NOT NULL",
-                        'salary_month_USD': "INT",
-                        'uni_code': "INT NOT NULL"})
-        data_base.insert_value(table_name='organization', var_name=('name, uni_code, department_uni_codes'), value=("'Corp', 12345, ARRAY[12345]"))
-        value = data_base.read_value(table_name='organization', var_name=('name'))
-        return True
-    except(Exception, psql.DatabaseError) as Error:
-            log.debug(f"DB Init Error...\n{Error}")
-            return False
-
-
-class Server:
-    def __init__(self, host: str, port: int, data_base: DB, max_clients: int):
-        self.db = data_base
-
-        self.main_socket = socket.socket()
-        self.main_socket.bind((host, port))
-        self.main_socket.listen(100)
-        self.main_socket.setblocking(False)
-
-        self.selector = selectors.DefaultSelector()
-        self.selector.register(fileobj=self.main_socket,
-                               events=selectors.EVENT_READ,
-                               data=self.on_accept)
-        
-        self.current_peers = {}
-
-    def on_accept(self, sock, mask):
-        try:
-            conn, addr = self.main_socket.accept()
-            log.info(f'accepted connection from {addr}')
-            conn.setblocking(False)
-            self.current_peers[conn.fileno()] = conn.getpeername()
-            self.selector.register(fileobj=conn, events=selectors.EVENT_READ,
-                                data=self.on_read)
-        except KeyError as Error:
-            log.debug(Error)
-
-    def close_connection(self, conn):
-
-        peername = self.current_peers[conn.fileno()]
-        log.info(f'closing connection to {peername}')
-        del self.current_peers[conn.fileno()]
-        conn.close()
-
-    def on_read(self, conn, mask):
-
-        try:
-            data = conn.recv(1000)
-            if data:
-                peername = conn.getpeername()
-                log.info(f'got data from {peername}: {data}')
-                conn.send(data)
-            else:
-                self.close_connection(conn)
-        except ConnectionResetError:
-            self.close_connection(conn)
-
-    def server_loop(self):
-        last_report_time = time.time()
-        while True:
-            events = self.selector.select(timeout=0.2)
-
-            for key, mask in events:
-                handler = key.data
-                handler(key.fileobj, mask)
-            cur_time = time.time()
-            if cur_time - last_report_time > 1:
-                log.info('Running report...')
-                log.info(f'Num active peers = {len(self.current_peers)}')
-                last_report_time = cur_time
+@on_request.connect
+def on_request(sender, **kw):
+    response = None
+    if kw['request']['action'] == 'read_table':
+        answer = sender.data_base.read_values(table_name='organization', var_name=('*'))
+        response = {"result": answer}
+    else:
+        response = {"result": f'Error: invalid action "{action}".'}
+    content_encoding = "utf-8"
+    response = {
+        "content_bytes": sender._json_encode(response, content_encoding),
+        "content_type": "text/json",
+        "content_encoding": content_encoding,
+    }
+    return response
 
 
 def main():
     db_tables = ('organization', 'department', 'person')
     data_base = DB(host="172.17.0.2", db_name="postgres",
-                   user="postgres", password="secret", db_tables=db_tables)    
-    db_init(data_base)
+                   user="postgres", password="secret", db_tables=db_tables)
+    server_addr = ('127.0.0.1', 8321)
+    loop = asyncio.get_event_loop()
+    server = ServerApp(server_addr, data_base)
+    loop.create_task(server.run_server())
+    loop.run_forever()
+    loop.close()
+
     #server = Server(host="127.0.0.1", port=8321, data_base=data_base, max_clients=10)
     #print(server)
     #server.server_loop()
